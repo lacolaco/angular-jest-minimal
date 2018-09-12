cb28777df5e63bded3e849c1c20b98b03c185d03
Suguru Inatomi <suguru.inatomi@gmail.com>
2018-09-13 08:40:58 +0900

    set up jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,20 +8,24 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  test('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'angular-jest-example'`, async(() => {
+  test(`should have as title 'angular-jest-example'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('angular-jest-example');
   }));
-  it('should render title in a h1 tag', async(() => {
+  test('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-jest-example!');
+  }));
+  test('should match snapshot', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    expect(fixture).toMatchSnapshot();
   }));
 });
